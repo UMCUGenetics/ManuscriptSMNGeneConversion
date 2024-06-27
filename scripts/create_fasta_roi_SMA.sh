8681c6a243a2ee0f515f2257e58faa52ad56392c
melferink <m.elferink@umcutrecht.nl>
2024-06-27 12:01:57 +0200

    refactor

--- scripts/create_fasta_roi_SMA.sh
+++ scripts/create_fasta_roi_SMA.sh
@@ -5,8 +5,7 @@
 #SBATCH --mem=50G
 #SBATCH --mail-type=ALL
 #SBATCH --ntasks=6
-#SBATCH --mail-user=mzwartk2@umcutrecht.nl
-#SBATCH --output=/hpc/dlab_haaften/mzwartkruis/Martin_C9/ONT_SMA_1000G_DATAFREEZE_20240223/slurm_output/create_fasta_roi_%j.out
+#SBATCH --output=create_fasta_roi_%j.out
 
 
 # Stop execution of script whenever a non-zero exit status is returned
@@ -19,21 +18,29 @@ Help()
   echo "Executing script to make fasta files for all haplotypes"
   echo
   echo
-  echo "Syntax: Input requirements [-h:o:i:r]"
+  echo "Syntax: Input requirements [-h:o:i:f:r:c:p:a]"
   echo "options:"
-  echo "h     Print help"
-  echo "o     Output directory, e.g. /hpc/dlab_haaften/mzwartkruis/Martin_C9/ONT_SMA_1000G_DATAFREEZE_20240223/analysis"
-  echo "i     Input directory, requires both SMA and 1000G subdirectory, e.g. /hpc/dlab_haaften/mzwartkruis/Martin_C9/ONT_SMA_1000G_DATAFREEZE_20240223/data"
-  echo "r     region of interest on chr5, e.g. 71274893-71447410"
+  echo "h     print help"
+  echo "o     output directory" 
+  echo "i     input directory, requires both SMA and 1000G subdirectory"
+  echo "f     full path to FASTA of reference contig, e.g. chr5.fa"
+  echo "r     region of interest on contig, e.g. 71274893-71447410"
+  echo "c     full path to copy_type_file"
+  echo "p     full path to SNVs_pivoted_paraphase file"
+  echo "a     dataset analysis type (e.g. SMA or 1000G"
 }
 
 ### Files required ###
-while getopts h:o:i:r: flag
+while getopts h:o:i:f:r:c:p:a: flag
 do
   case "${flag}" in
     o) output_dir=${OPTARG};;
     i) input_dir=${OPTARG};;
+    f) contig_fasta=${OPTARG};;
     r) ROI=${OPTARG};;
+    c) copy_type_file=${OPTARG};;
+    p) pivot_file=${OPTARG};;
+    a) analysis=${OPTARG};;
     h) # display Help
         Help
         exit 1;;
@@ -44,21 +51,25 @@ do
 done
 
 #define tools
-samtools="singularity exec -B /hpc/:/hpc/ -B $TMPDIR:$TMPDIR /hpc/dlab_haaften/dgommers/tools/samtools:1.17--hd87286a_1.img samtools"
+samtools=/path/to/samtools
 
+## Repository wkdir
+repo_folder="$(dirname "$(readlink -f "$0")")"
+
+## Load virtual venv
+source ${repo_folder}/venv/bin/activate
 
 #Part 1
 #Creating depth files of region of interest
 #SMA
-mkdir -p "${output_dir}"
-mkdir -p "${output_dir}/SMA/"
-mkdir -p "${output_dir}/SMA/fasta_haplotypes"
-mkdir -p "${output_dir}/SMA/fasta_haplotypes/depth_files"
+mkdir -p "${output_dir}/${analysis}/fasta_haplotypes/depth_files"
 
 echo "Creating depth files per sample haplotype".
-echo ${input_dir}/SMA/{SMA,HG}*/bam_files_haplotagged_split
+echo ${input_dir}/${analysis}/{SMA,HG}*/bam_files_haplotagged_split
 # Iterate over directories
-for dir in ${input_dir}/SMA/{SMA,HG}*/bam_files_haplotagged_split; do
+
+
+for dir in ${input_dir}/${analysis}/{SMA,HG}*/bam_files_haplotagged_split; do
   if [ -d "${dir}" ]; then
     echo "Processing files in directory: ${dir}"
 
@@ -74,37 +85,35 @@ for dir in ${input_dir}/SMA/{SMA,HG}*/bam_files_haplotagged_split; do
 
 
       #Run samtools depth for every bam file
-      $samtools depth -J -H --min-MQ 5 -aa -r "chr5:${ROI}" -o "${output_dir}/SMA/fasta_haplotypes/depth_files/${sample_name}_${hap}_depth_all_pos_roi.bed" ${bam_file_path}
-      echo "Samtools depth for ${sample_name}_${hap} completed on ROI"
+      $samtools depth -J -H --min-MQ 5 -aa -r "chr5:${ROI}" -o "${output_dir}/${analysis}/fasta_haplotypes/depth_files/${sample_name}_${hap}_depth_all_pos_roi.bed" ${bam_file_path}
+      echo "samtools depth for ${sample_name}_${hap} completed on ROI"
       #add correct header
-      awk 'BEGIN {OFS="\t"} NR==1 {print "CHROM", "POS", "depth"; next} {print}' "${output_dir}/SMA/fasta_haplotypes/depth_files/${sample_name}_${hap}_depth_all_pos_roi.bed" > "${output_dir}/SMA/fasta_haplotypes/depth_files/${sample_name}_${hap}_depth_all_pos_roi_header.bed"
+      awk 'BEGIN {OFS="\t"} NR==1 {print "CHROM", "POS", "depth"; next} {print}' "${output_dir}/${analysis}/fasta_haplotypes/depth_files/${sample_name}_${hap}_depth_all_pos_roi.bed" > "${output_dir}/${analysis}/fasta_haplotypes/depth_files/${sample_name}_${hap}_depth_all_pos_roi_header.bed"
       #remove files without header
-      rm "${output_dir}/SMA/fasta_haplotypes/depth_files/${sample_name}_${hap}_depth_all_pos_roi.bed"
+      rm "${output_dir}/${analysis}/fasta_haplotypes/depth_files/${sample_name}_${hap}_depth_all_pos_roi.bed"
 
       fi
     done
   else
-  echo "not right directory given"
+  echo "wrong directory provided as input"
   fi
 done
 
 
 #Part 2
-#Preparing variant input files for fasta maker script
-mkdir -p "${output_dir}/SMA/fasta_haplotypes/pos_ref_alt_files"
-cd "${output_dir}/SMA/fasta_haplotypes/pos_ref_alt_files"
+mkdir -p "${output_dir}/${analysis}/fasta_haplotypes/pos_ref_alt_files"
+cd "${output_dir}/${analysis}/fasta_haplotypes/pos_ref_alt_files"
 
-/hpc/dlab_haaften/mzwartkruis/Martin_C9/ONT_SMA_1000G_DATAFREEZE_20240223/scripts/./prep_variant_input_fasta_maker.py ${output_dir}/SMA/SNVs_pivoted_paraphase_suppl_made_in_R.xlsx pos_ref_alt_
+$repo_folder/prep_variant_input_fasta_maker.py $pivot_file pos_ref_alt_
 
 #Part 3
 #making fasta files of all haplotypes
-chr5_fa="/hpc/dlab_haaften/mzwartkruis/Martin_C9/ONT_SMA_1000G_DATAFREEZE_20240223/scripts/chr5.fa"
 
-mkdir -p "${output_dir}/SMA/fasta_haplotypes/fasta"
-mkdir -p "${output_dir}/SMA/fasta_haplotypes/log"
+mkdir -p "${output_dir}/${analysis}/fasta_haplotypes/fasta"
+mkdir -p "${output_dir}/${analysis}/fasta_haplotypes/log"
 
 # Iterate over haplotypes
-for alt_file_path in ${output_dir}/SMA/fasta_haplotypes/pos_ref_alt_files/*.tsv; do
+for alt_file_path in ${output_dir}/${analysis}/fasta_haplotypes/pos_ref_alt_files/*.tsv; do
   if [ -f "${alt_file_path}" ]; then
     echo "Processing file: ${alt_file_path}"
     alt_file=$(basename $alt_file_path)
@@ -114,26 +123,24 @@ for alt_file_path in ${output_dir}/SMA/fasta_haplotypes/pos_ref_alt_files/*.tsv;
     echo "${hap}"
     echo "Creating a fasta file for ${sample_name} ${hap}"
     # Execute python script
-    python3 /hpc/dlab_haaften/mzwartkruis/Martin_C9/ONT_SMA_1000G_DATAFREEZE_20240223/scripts/./creating_new_fasta_single.py ${chr5_fa} ${alt_file_path} ${ROI} ${output_dir}/SMA/fasta_haplotypes/fasta/${sample_name}_${hap}.fa ${output_dir}/SMA/fasta_haplotypes/log/${sample_name}_${hap}.txt ${output_dir}/SMA/fasta_haplotypes/depth_files/*${sample_name}_${hap}*.bed ${sample_name}_${hap}
-    #usage: /hpc/dlab_haaften/dgommers/scripts/./creating_new_fasta_demi.py <reference_fasta> <pos_ref_alt_file.tsv> <ROI, eg. 71274893-71447410> <output_name.fa> <logfile_name.txt> <depth_file.bed> <sample_hap>
+    python3 $repo_folder/creating_new_fasta_single.py ${contig_fasta} ${alt_file_path} ${ROI} ${output_dir}/${analysis}/fasta_haplotypes/fasta/${sample_name}_${hap}.fa ${output_dir}/${analysis}/fasta_haplotypes/log/${sample_name}_${hap}.txt ${output_dir}/${analysis}/fasta_haplotypes/depth_files/*${sample_name}_${hap}*.bed ${sample_name}_${hap}
   fi
 done
 
 
 #Part 4
 #Concatenate fasta files per copy type (SMN1 or SMN2)
-copy_type_file=/hpc/dlab_haaften/mzwartkruis/Martin_C9/ONT_SMA_1000G_DATAFREEZE_20240223/analysis/SMA/list_haplotypes_copy_type.tsv
 
 #exclude header
-tail -n +2 ${copy_type_file} > ${output_dir}/SMA/fasta_haplotypes/fasta/sample_names_file.txt
+tail -n +2 ${copy_type_file} > ${output_dir}/${analysis}/fasta_haplotypes/fasta/sample_names_file.txt
 
 #Making sure that there are no empty lines in the file
-tr -d '\r' < ${output_dir}/SMA/fasta_haplotypes/fasta/sample_names_file.txt > ${output_dir}/SMA/fasta_haplotypes/fasta/sample_names_file_unix.txt
+tr -d '\r' < ${output_dir}/${analysis}/fasta_haplotypes/fasta/sample_names_file.txt > ${output_dir}/${analysis}/fasta_haplotypes/fasta/sample_names_file_unix.txt
 
 # Make directories for SMN1 and SMN2
-mkdir -p ${output_dir}/SMA/fasta_haplotypes/fasta/SMN1
-mkdir -p ${output_dir}/SMA/fasta_haplotypes/fasta/SMN2
-mkdir -p ${output_dir}/SMA/fasta_haplotypes/fasta/merged
+mkdir -p ${output_dir}/${analysis}/fasta_haplotypes/fasta/SMN1
+mkdir -p ${output_dir}/${analysis}/fasta_haplotypes/fasta/SMN2
+mkdir -p ${output_dir}/${analysis}/fasta_haplotypes/fasta/merged
 
 while IFS= read -r line; do
   echo $line
@@ -148,23 +155,16 @@ while IFS= read -r line; do
 
 
   if [ -n "$sample" ] && [ -n "$smn_copy" ] && [ "$smn_copy" != "NA" ]; then
-    cp "${output_dir}/SMA/fasta_haplotypes/fasta/${sample}.fa" "${output_dir}/SMA/fasta_haplotypes/fasta/${smn_copy}"
+    cp "${output_dir}/${analysis}/fasta_haplotypes/fasta/${sample}.fa" "${output_dir}/${analysis}/fasta_haplotypes/fasta/${smn_copy}"
   else
     echo "Invalid sample or SMN copy."
   fi
 
 
-done < ${output_dir}/SMA/fasta_haplotypes/fasta/sample_names_file_unix.txt
-
-# Removing merged fastas if they already exist
-#if [ -d ${output_dir}/SMA/fasta_haplotypes/fasta/merged/SMN1_haplotypes.fa]; then
-#  rm ${output_dir}/SMA/fasta_haplotypes/fasta/merged/SMN2_haplotypes.fa
-#else
-#  continue
-#fi
+done < ${output_dir}/${analysis}/fasta_haplotypes/fasta/sample_names_file_unix.txt
 
 # Concatenating into haplotype files
-cat ${output_dir}/SMA/fasta_haplotypes/fasta/SMN1/* >> ${output_dir}/SMA/fasta_haplotypes/fasta/merged/SMN1_haplotypes.fa
-cat ${output_dir}/SMA/fasta_haplotypes/fasta/SMN2/* >> ${output_dir}/SMA/fasta_haplotypes/fasta/merged/SMN2_haplotypes.fa
+cat ${output_dir}/${analysis}/fasta_haplotypes/fasta/SMN1/* >> ${output_dir}/${analysis}/fasta_haplotypes/fasta/merged/SMN1_haplotypes.fa
+cat ${output_dir}/${analysis}/fasta_haplotypes/fasta/SMN2/* >> ${output_dir}/${analysis}/fasta_haplotypes/fasta/merged/SMN2_haplotypes.fa
 
 echo "End of script"

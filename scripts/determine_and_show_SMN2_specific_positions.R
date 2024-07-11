e5bd0744b2f17359d2a9f412cfa41b670ce2164f
melferink <m.elferink@umcutrecht.nl>
2024-07-11 15:50:20 +0200

    included smn-type for output

--- scripts/determine_and_show_SMN2_specific_positions.R
+++ scripts/determine_and_show_SMN2_specific_positions.R
@@ -7,6 +7,7 @@ args = commandArgs(trailingOnly=TRUE)
 SNV_table = read_delim(args[1], delim = "\t")
 PSVs = read_delim(args[2], delim = "\t")
 prefix = args[3]
+smn_type = args[4]
 
 
 ###PROCESSING SNVs###
@@ -63,9 +64,9 @@ SNV_tally_SMN_filtered <- SNV_tally_SMN %>%
   filter(!is.na(copy_specific)) %>%
   filter(SMN1_tot_haps_non_NA >= 20 & SMN2_tot_haps_non_NA >= 20)
 
-SMN2_specific_positions_bed <- SNV_tally_SMN_filtered %>%
+SMN_specific_positions_bed <- SNV_tally_SMN_filtered %>%
   ungroup() %>%
-  filter(copy_specific == "SMN2-env") %>% #filter for only SMN2-specific positions
+  filter(copy_specific == smn_type) %>% #filter for only smn_type specific positions
   mutate(annotation = case_when(!is.na(PSV) ~ PSV,
                                 is.na(PSV) ~ copy_specific)) %>%
   mutate(chr = "chr5") %>%
@@ -73,23 +74,23 @@ SMN2_specific_positions_bed <- SNV_tally_SMN_filtered %>%
   mutate(stop = POS) %>%
   select(chr, start, stop, annotation)
 
-#write bed files with SMN2 specific positions
-write.table(SMN2_specific_positions_bed, paste(prefix, "_SMN2_specific_positions_SMN2_0.9_SMN1_0.2.bed", sep = ""), col.names = FALSE, row.names = FALSE, sep="\t", quote=FALSE)
+#write bed files with SMN specific positions
+write.table(SMN_specific_positions_bed, paste(prefix, "_", smn_type, "_specific_positions_0.9_SMN1_0.2.bed", sep = ""), col.names = FALSE, row.names = FALSE, sep="\t", quote=FALSE)
 
-#Look at SNVs at SMN2-specific positions, but for SMN2_specific_positions_bed
-colnames(SMN2_specific_positions_bed) <- c("chr", "start", "POS", "annotation")
-SNV_at_SMN2_specific_positions <- SNV_table %>%
-  filter(POS %in% SMN2_specific_positions_bed$POS) %>% #filter for SMN2-specific positions
+#Look at SNVs at smn_type specific positions, but for specific_positions_bed
+colnames(SMN_specific_positions_bed) <- c("chr", "start", "POS", "annotation")
+SNV_at_SMN_specific_positions <- SNV_table %>%
+  filter(POS %in% SMN_specific_positions_bed$POS) %>% #filter for smn_type specific positions
   mutate(environment = case_when(POS != 71407128 & genotype_checked == "ALT" ~ 2,
                                  POS != 71407128 & genotype_checked == "REF" ~ 1,
                                  POS == 71407128 & genotype_checked == "ALT" ~ 1,
                                  POS == 71407128 & genotype_checked == "REF" ~ 2)) #PSV5 behaves differently because it is a hybrid PSV in reference genome T2T-CHM13
 
-#pivot the table to show all SNVs next to each other at SMN2-specific positions
-SNV_at_SMN2_specific_positions_pivoted <- SNV_at_SMN2_specific_positions %>%
+#pivot the table to show all SNVs next to each other at SMN-specific positions
+SNV_at_SMN_specific_positions_pivoted <- SNV_at_SMN_specific_positions %>%
   select(sample_name, hap, POS, SMN_copy_type, environment) %>%
   arrange(POS) %>%
   pivot_wider(names_from = POS, values_from = environment) %>%
   arrange(sample_name, hap)
-write_tsv(SNV_at_SMN2_specific_positions_pivoted, paste(prefix, "_SNVs_at_SMN2_specific_positions.tsv", sep = ""), quote="none", na="")
+write_tsv(SNV_at_SMN_specific_positions_pivoted, paste(prefix, "_", smn_type, "_at_SMN2_specific_positions.tsv", sep = ""), quote="none", na="")
 
